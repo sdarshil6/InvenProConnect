c1332fe39d270ff346c0501bd1ee123ad739b00c
Darshil Shah <sdarshil786@gmail.com>
2024-05-16 22:39:06 +0530

    organize mapping configurations in AutoMapperProfile

---
+++ Helpers/AutoMapperProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using InvenProConnect.DTOs;
+using InvenProConnect.Models;
+
+namespace InvenProConnect.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, GetAllUsersDto>().ReverseMap();
+            CreateMap<User, GetUserDto>().ReverseMap();
+            CreateMap<User, InsertUserDto>().ReverseMap();
+            CreateMap<User, UpdateUserDto>().ReverseMap();
+        }
+    }
+}

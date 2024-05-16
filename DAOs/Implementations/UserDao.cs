f173c431fbe2a7ec8cf4f240238d3bd5684373cd
Darshil Shah <sdarshil786@gmail.com>
2024-05-16 22:44:29 +0530

    Creating DAO for entity (user)

---
+++ DAOs/Implementations/UserDao.cs
@@ -0,0 +1,39 @@
+﻿using InvenProConnect.DAOs.Interfaces;
+using InvenProConnect.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InvenProConnect.DAOs.Implementations
+{
+    public class UserDao : BaseDao<User>, IUserDao
+    {
+        public UserDao(InventoryManagementSystemContext context) : base(context) 
+        {
+
+        }
+
+        public void DeleteUser(User user)
+        {
+            Delete(user);
+        }
+
+        public async Task<IEnumerable<User>> GetAllUsersAsync()
+        {
+            return await FindAll().ToListAsync();
+        }
+
+        public async Task<User> GetUserByIdAsync(long id)
+        {
+            return await FindByCondition(u => u.UserId == id).FirstOrDefaultAsync();
+        }
+
+        public void InsertUser(User user)
+        {
+            Create(user);
+        }
+
+        public void UpdateUser(User user)
+        {
+            Update(user);
+        }
+    }
+}
---
+++ DAOs/Interfaces/IUserDao.cs
@@ -0,0 +1,13 @@
+﻿using InvenProConnect.Models;
+
+namespace InvenProConnect.DAOs.Interfaces
+{
+    public interface IUserDao : IBaseDao<User>
+    {
+        Task<IEnumerable<User>> GetAllUsersAsync();
+        Task<User> GetUserByIdAsync (long id);
+        void InsertUser (User user);
+        void UpdateUser (User user);
+        void DeleteUser(User user);
+    }
+}

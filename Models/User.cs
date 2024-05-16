5bea9c237038b3f1b949254b12ee007540c52ce2
Darshil Shah <sdarshil786@gmail.com>
2024-05-16 22:41:20 +0530

    Creating entity class (User.cs) using Scaffold-DbContext command

---
+++ Models/User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InvenProConnect.Models;
+
+public partial class User
+{
+    public long Id { get; set; }
+
+    public long UserId { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string? LastName { get; set; }
+
+    public string Password { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public int? Age { get; set; }
+
+    public string? Gender { get; set; }
+
+    public string Role { get; set; } = null!;
+
+    public long? Salary { get; set; }
+
+    public DateOnly? JoinDate { get; set; }
+
+    public DateOnly? BirthDate { get; set; }
+
+    public string Phone { get; set; } = null!;
+
+    public string? HomeTown { get; set; }
+
+    public string? CurrentCity { get; set; }
+
+    public string? BloodGroup { get; set; }
+
+    public string? PostalCodeHomeTown { get; set; }
+
+    public string? PostalCodeCurrentCity { get; set; }
+}

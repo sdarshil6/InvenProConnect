3288792fd228653532ef0fe7a88ce49134f2d1e5
Darshil Shah <sdarshil786@gmail.com>
2024-05-16 22:35:47 +0530

    Creating entity & context classes for an existing database (InventoryManagementSystem) using Database-First approach (Scaffold-DbContext command)

---
+++ Models/InventoryManagementSystemContext.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace InvenProConnect.Models;
+
+public partial class InventoryManagementSystemContext : DbContext
+{
+    public InventoryManagementSystemContext()
+    {
+    }
+
+    public InventoryManagementSystemContext(DbContextOptions<InventoryManagementSystemContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=LAPTOP-14RMMG4E\\SQLEXPRESS;Database=InventoryManagementSystem;Trusted_Connection=True;TrustServerCertificate=True;Integrated Security=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Users__3214EC075516B60F");
+
+            entity.HasIndex(e => e.UserId, "UQ__Users__1788CC4D81F03170").IsUnique();
+
+            entity.HasIndex(e => e.Email, "UQ__Users__A9D10534344C05C7").IsUnique();
+
+            entity.Property(e => e.BloodGroup)
+                .HasMaxLength(5)
+                .IsUnicode(false);
+            entity.Property(e => e.CurrentCity)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Email)
+                .HasMaxLength(200)
+                .IsUnicode(false);
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(500)
+                .IsUnicode(false);
+            entity.Property(e => e.Gender)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.HomeTown)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.LastName)
+                .HasMaxLength(500)
+                .IsUnicode(false);
+            entity.Property(e => e.Password)
+                .HasMaxLength(500)
+                .IsUnicode(false);
+            entity.Property(e => e.Phone)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.PostalCodeCurrentCity)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.PostalCodeHomeTown)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.Role)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}

767b1d5ac26511526fd279615591a95fe3276bfb
Darshil Shah <sdarshil786@gmail.com>
2024-05-16 22:43:01 +0530

    Creating necessary DTOs for entity (User)

---
+++ DTOs/UpdateUserDto.cs
@@ -0,0 +1,34 @@
+﻿namespace InvenProConnect.DTOs
+{
+    public class UpdateUserDto
+    {
+        public string FirstName { get; set; } = null!;
+
+        public string? LastName { get; set; }
+
+        public string Password { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+        public string? Gender { get; set; }
+
+        public string Role { get; set; } = null!;
+
+        public long? Salary { get; set; }
+
+        public DateOnly? JoinDate { get; set; }
+
+        public DateOnly? BirthDate { get; set; }
+
+        public string Phone { get; set; } = null!;
+
+        public string? HomeTown { get; set; }
+
+        public string? CurrentCity { get; set; }
+
+        public string? BloodGroup { get; set; }
+
+        public string? PostalCodeHomeTown { get; set; }
+
+        public string? PostalCodeCurrentCity { get; set; }
+    }
+}
